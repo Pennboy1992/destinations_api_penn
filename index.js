3b78a910c26f78f2a4d7d6da799bbc12121fdc48
Roman Penn <pnnroman92@gmail.com>
2022-02-10 17:33:02 -0600

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { destinations } = require("./db");
 
 const express = require("express");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 const server = express();
+
+server.use(cors());
 
 server.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
