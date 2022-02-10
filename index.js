5834a61f4bec10a5883277e2ed38d09a42812659
Roman Penn <pnnroman92@gmail.com>
2022-02-10 16:55:13 -0600

    setting up dynamic ports

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { destinations } = require("./db");
 
 const express = require("express");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const server = express();
 
 server.listen(PORT, () => {
